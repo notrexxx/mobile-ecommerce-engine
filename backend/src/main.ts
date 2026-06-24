9b6043593e0c7821764ae84f7a0d17f63f911e29
Leon <notrexx15@gmail.com>
2026-06-24 01:32:43 -0400

    chore: enable CORS in nestjs backend for vercel integration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,13 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
   app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
